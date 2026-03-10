0b2c03aa977660656bc84b11914e6d19257cd651
bojeda12 <brandon05251997@gmail.com>
2026-03-09 20:35:14 -0600

    sea ctualiza mensaje de phyton

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Pruba superada"
+    return Modificacion para nuestro railway, todo funciona!!!"
 if __name__ == "__main__":
     app.run()
