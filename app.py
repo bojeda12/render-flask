9b5c57d2de5e2ddf2b0a336522e8c122f8d21b6b
bojeda12 <brandon05251997@gmail.com>
2026-03-12 20:09:43 -0600

    Fix typo in home route response message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API funcionando con Docker!!!"
+    return "API funcionand con Docker!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
