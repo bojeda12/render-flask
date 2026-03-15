425264c4edc5e30a6c934f5544b4800e2e70ef9a
bojeda12 <brandon05251997@gmail.com>
2026-03-15 13:09:39 -0600

    actualizamos mensaje de bienvenida

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API funcionando con Azure!"
+    return "API funcionando con Azure,Actualizado!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
